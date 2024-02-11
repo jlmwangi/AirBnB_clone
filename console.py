40fe87c77796bc7421b329a8c91ca68e21f22c6f
Mwangii <jlmwangi254@gmail.com>
2024-02-11 15:09:50 +0300

    modify the console

--- console.py
+++ console.py
@@ -2,6 +2,11 @@
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
 import cmd
 
 
